6c94092a4746107798cf86fd9785be68df6fbeea
kevkhv <kevkhv@yandex.ru>
2022-02-13 23:31:06 +0300

    create obj  Factory,CarShowroom

--- src/com/homeWork/Main.java
+++ src/com/homeWork/Main.java
@@ -6,7 +6,9 @@ public class Main {
         Car mark2 = new Car("Белый","Toyota Mark2", 1996,17, 3.0, "2jzgte", "Automatic gettrag");
         System.out.println(mark2); // выведем на печать нашу машину
 
-        Service service = new Service();// создали обьект завода
+        Service service = new Service();// создали обьект сервиса
+        Factory factory = new Factory(); // создали обьект завода
+        CarShowroom carShowroom = new CarShowroom();// создали обьект салона
 
         service.setColor("Красный",mark2); // отдаем заводу конкретную машину на перекраску и вызываем метод завода
         service.setWheels(14,mark2); // отдаем заводу конкретную машину на изменение размера колес и вызываем метод завода
